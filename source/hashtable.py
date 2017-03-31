2396008ee9f10a960e252fd0be5abc33ab2a5f30
Alan Davis <neptunius@gmail.com>
2017-03-31 11:30:40 -0700

    Add hash table implementation and unit tests

---
+++ source/hashtable.py
@@ -0,0 +1,145 @@
+#!python
+
+from linkedlist import LinkedList
+
+
+class HashTable(object):
+
+    def __init__(self, init_size=8):
+        """Initialize this hash table with the given initial size"""
+        self.buckets = [LinkedList() for i in range(init_size)]
+
+    def __str__(self):
+        """Return a formatted string representation of this hash table"""
+        items = ['{}: {}'.format(repr(k), repr(v)) for k, v in self.items()]
+        return '{' + ', '.join(items) + '}'
+
+    def __repr__(self):
+        """Return a string representation of this hash table"""
+        return 'HashTable({})'.format(repr(self.items()))
+
+    def _bucket_index(self, key):
+        """Return the bucket index where the given key would be stored"""
+        return hash(key) % len(self.buckets)
+
+    def keys(self):
+        """Return a list of all keys in this hash table"""
+        # Collect all keys in each of the buckets
+        all_keys = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_keys.append(key)
+        return all_keys
+
+    def values(self):
+        """Return a list of all values in this hash table"""
+        # Collect all values in each of the buckets
+        all_values = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_values.append(value)
+        return all_values
+
+    def items(self):
+        """Return a list of all entries (key-value pairs) in this hash table"""
+        # Collect all pairs of key-value entries in each of the buckets
+        all_items = []
+        for bucket in self.buckets:
+            all_items.extend(bucket.items())
+        return all_items
+
+    def length(self):
+        """Return the length of this hash table by traversing its buckets"""
+        # Count number of key-value entries in each of the buckets
+        item_count = 0
+        for bucket in self.buckets:
+            item_count += bucket.length()
+        return item_count
+        # Equivalent to this list comprehension:
+        return sum(bucket.length() for bucket in self.buckets)
+
+    def contains(self, key):
+        """Return True if this hash table contains the given key, or False"""
+        # Find the bucket the given key belongs in
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        # Check if an entry with the given key exists in that bucket
+        entry = bucket.find(lambda (k, v): k == key)
+        return entry is not None  # True or False
+
+    def get(self, key):
+        """Return the value associated with the given key, or raise KeyError"""
+        # Find the bucket the given key belongs in
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        # Find the entry with the given key in that bucket, if one exists
+        entry = bucket.find(lambda (k, v): k == key)
+        if entry is not None:  # Found
+            # Return the given key's associated value
+            assert isinstance(entry, tuple)
+            assert len(entry) == 2
+            return entry[1]
+        else:  # Not found
+            raise KeyError('Key not found: {}'.format(key))
+
+    def set(self, key, value):
+        """Insert or update the given key with its associated value"""
+        # Find the bucket the given key belongs in
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        # Find the entry with the given key in that bucket, if one exists
+        # Check if an entry with the given key exists in that bucket
+        entry = bucket.find(lambda (k, v): k == key)
+        if entry is not None:  # Found
+            # In this case, the given key's value is being updated
+            # Remove the old key-value entry from the bucket first
+            bucket.delete(entry)
+        # Insert the new key-value entry into the bucket in either case
+        bucket.append((key, value))
+
+    def delete(self, key):
+        """Delete the given key and its associated value, or raise KeyError"""
+        # Find the bucket the given key belongs in
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        # Find the entry with the given key in that bucket, if one exists
+        entry = bucket.find(lambda (k, v): k == key)
+        if entry is not None:  # Found
+            # Remove the key-value entry from the bucket
+            bucket.delete(entry)
+        else:  # Not found
+            raise KeyError('Key not found: {}'.format(key))
+
+
+def test_hash_table():
+    ht = HashTable()
+    print('HashTable: ' + str(ht))
+
+    print('Setting entries:')
+    ht.set('I', 1)
+    print('set(I, 1): ' + str(ht))
+    ht.set('V', 5)
+    print('set(V, 5): ' + str(ht))
+    ht.set('X', 10)
+    print('set(X, 10): ' + str(ht))
+    print('length: ' + str(ht.length()))
+
+    print('Getting entries:')
+    print('get(I): ' + str(ht.get('I')))
+    print('get(V): ' + str(ht.get('V')))
+    print('get(X): ' + str(ht.get('X')))
+    print('contains(X): ' + str(ht.contains('X')))
+
+    print('Deleting entries:')
+    ht.delete('I')
+    print('delete(I): ' + str(ht))
+    ht.delete('V')
+    print('delete(V): ' + str(ht))
+    ht.delete('X')
+    print('delete(X): ' + str(ht))
+    print('contains(X): ' + str(ht.contains('X')))
+    print('length: ' + str(ht.length()))
+
+
+if __name__ == '__main__':
+    test_hash_table()
